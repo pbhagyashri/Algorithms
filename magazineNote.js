d567254ec341b23ff3d3b0d4958d58e97733af4b
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-06-30 03:37:12 -0400

    Add code to magazineNote.js to check if 2 arrays contain same strings same number of times

--- magazineNote.js
+++ magazineNote.js
@@ -1,4 +1,41 @@
+var magazine = ['give', 'me', 'one', 'grand', 'today', 'night']
+var note = ['give', 'one', 'grand', 'today', 'one']
+
 function checkMagazine(magazine, note) {
+ 
+  let magazineObj = {};
+  let noteObj = {}
+  let maxLength = 0
+  magazine.length > note.length ? maxLength = magazine.length : maxLength = note.length
 
-
+  let i = 0
+  while (i < maxLength) {
+ 
+    if (i < magazine.length){
+    	magazineObj[magazine[i]] ? magazineObj[magazine[i]] += 1 : magazineObj[magazine[i]] = 1
+    }
+    
+    if(i < note.length) {
+    	noteObj[note[i]] ? noteObj[note[i]] += 1 : noteObj[note[i]] = 1
+    }
+    
+    i++
+  }
+	
+  let result = ''
+  
+  for (let j in noteObj) {
+  	if (result == 'No') {
+    	console.log(result)
+    	return
+    } 
+    if(noteObj[j] === magazineObj[j]) {
+      result = 'Yes'
+    } else {
+      result = 'No'
+    }
+  }
+  console.log(result)
 }
+
+checkMagazine(magazine, note)
