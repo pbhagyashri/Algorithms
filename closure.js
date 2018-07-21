22808786b024b0a383560697a480ba9934e9e846
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-07-21 16:14:24 -0400

    Add closure.js

---
+++ closure.js
@@ -0,0 +1,13 @@
+var addTo = function(passed) {
+
+  var add = function(inner) {
+    return passed + inner
+  }
+  return add;
+}
+
+var addThree = new addTo(3)
+var addFour = new addTo(4)
+
+console.log(addThree(1))
+console.log(addFour(1))
