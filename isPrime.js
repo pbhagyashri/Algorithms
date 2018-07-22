4bc0b763948fa46f1a8870ae05c28ca9430bd202
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-07-22 15:08:14 -0400

    Add is Prime.js

---
+++ isPrime.js
@@ -0,0 +1,17 @@
+function isPrime(n) {
+  var divisor = 2;
+
+  while (n > divisor) {
+
+    if (n % divisor == 0) {
+      return false
+    }else {
+      divisor++
+    }
+    
+  }
+  return true
+}
+
+console.log(isPrime(137))
+console.log(isPrime(237))
